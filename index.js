3117225d7358b087ed61f626e26b4f89d8453986
sasszz <luchevreuil@gmail.com>
2023-05-11 17:04:23 -0700

    Init Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let url =
+  "https://open.spotify.com/playlist/6yd5pnsXiR76dZkug2vAc0?si=b3f3cf8027884e16";
+let result_id = url.substring(34, 56);
+
+let id = "6yd5pnsXiR76dZkug2vAc0"; // 22 characters long
+
+console.log(id);
+console.log(result_id === id);
